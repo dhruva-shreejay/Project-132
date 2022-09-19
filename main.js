7bd2b46d9f34dc3d18251a44fe0d89697ade7fd1
dhruva-shreejay <77469504+dhruva-shreejay@users.noreply.github.com>
2022-09-19 08:31:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+img = "";
+status = "";
+
+function preload()
+{
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById().innerHTML = "Status : Detecting Objects";
+
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error) {
+        console.log(error);
+    }
+
+    console.log(results);
+}
